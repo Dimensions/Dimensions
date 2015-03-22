9b2a4b6ae84070992f2ee6b011325db1ca2df221
Jαdon Fowler <ph4sesaber@gmail.com>
2015-03-22 14:28:54 -0700

    Add net/ to buildPatches

--- buildPatches.sh
+++ buildPatches.sh
@@ -1,6 +1,6 @@
 #Build patch files from edited NMS source code
 
-diff -rupN mcp/src/old_nms mcp/src/minecraft_server/ > direct/nms.patch
+diff -rupN mcp/src/old_nms/net/ mcp/src/minecraft_server/net/ > direct/nms.patch
 #TODO: Find a way to have patches for each file
 
 echo Patches created in direct/nms.patch
