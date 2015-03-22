121479241c9f774cd052256ae7c149e8791e352f
Jαdon Fowler <ph4sesaber@gmail.com>
2015-03-22 13:43:10 -0700

    Add echos to applyPatches

--- init.sh
+++ init.sh
@@ -3,8 +3,12 @@
 #Copy NMS source to compare for patches later
 cp -a mcp/src/minecraft_server/ mcp/src/old_nms
 
+echo Done copying NMS source
+
 #Copy indirect code into NMS
 cp indirect/ mcp/src/minecraft_server/
+
+echo Done moving indirect code into NMS
 
 #Apply the direct patches to NMS
 applyPatches.sh
