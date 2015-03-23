171ef526c94760e0447304e255a0c980ed584757
Phase <ph4sesaber@gmail.com>
2015-03-22 22:26:10 -0700

    Eclipse setup script

---
+++ eclipse.sh
@@ -0,0 +1,6 @@
+mkdir -p mcp/eclipse/Server/src
+cp -a mcp/src/minecraft_server/net mcp/eclipse/Server/src
+cp -a mcp/src/minecraft_server/dimensions mcp/eclipse/Server/src
+cp -a mcp/jars mcp/eclipse/Server
+
+echo Files moved into eclipse/Server/
