2a324afc1068f57608c9d8049956c9c7d9075ae4
Phase <ph4sesaber@gmail.com>
2015-03-22 21:28:45 -0700

    Fix init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,31 @@
 #This file is to be run before you do anything
 
+echo This is the initialization script for Dimensions.
+
 #Copy NMS source to compare for patches later
+mkdir -p mcp/src/old_nms
 cp -a mcp/src/minecraft_server/net/ mcp/src/old_nms/net/
 
 echo Done copying NMS source
 
+#Set up Solar submodule
+cd Solar
+git submodule init
+git submodule update
+cd ..
+
+echo Solar set up
+
 #Copy Solar API into NMS
-cp Solar/indirect/ mcp/src/minecraft_server/
+cp -a Solar/indirect/dimensions mcp/src/minecraft_server/
 
 echo Done moving Solar code into NMS
 
 #Copy indirect code into NMS
-cp indirect/ mcp/src/minecraft_server/
+mkdir -p indirect/dimensions
+cp -a indirect/dimensions/ mcp/src/minecraft_server/
 
 echo Done moving indirect code into NMS
 
 #Apply the direct patches to NMS
-applyPatches.sh
+#applyPatches.sh
