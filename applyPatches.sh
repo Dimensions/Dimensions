06cee12cf7433fd86c5b81e2add82b53ef4fdad7
Phase <ph4sesaber@gmail.com>
2015-03-23 00:13:14 -0700

    Fixing patching

--- applyPatches.sh
+++ applyPatches.sh
@@ -1,4 +1,4 @@
-#This applys the direct patches to your local mcp directory
+#This applies the direct patches to your local mcp directory
 #This uses diff & patch, not git diff
 
 #Change directory to direct, so we can get the patches
@@ -10,10 +10,10 @@ for patch in *.patch
 do
 
   #Go to the NMS folder from MCP
-  cd ../mcp/src/minecraft_server/net/
+  cd ../
 
   #Apply patches to NMS (Will this work it it's not in the same directory?)
-  patch -p6 < patch
+  patch -p0 < direct/$patch
   echo Applied $patch
 
 done
--- buildPatches.sh
+++ buildPatches.sh
@@ -1,7 +1,7 @@
 #Build patch files from edited NMS source code
 
 mkdir -p direct
-diff -rupN mcp/src/old_nms/net/ mcp/src/minecraft_server/net/ > direct/nms.patch
+diff -rupNd mcp/src/old_nms/net/ mcp/src/minecraft_server/net/ > direct/nms.patch
 #TODO: Find a way to have patches for each file
 
 echo Patches created in direct/nms.patch
