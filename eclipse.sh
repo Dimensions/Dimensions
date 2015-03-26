31dc5017b01c8991b33497c0b5c169d277199092
phase <ph4sesaber@gmail.com>
2015-03-26 08:46:12 -0700

    Delete eclipse.sh

--- eclipse.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p mcp/eclipse/Server/src
-cp -a mcp/src/minecraft_server/net mcp/eclipse/Server/src
-cp -a mcp/src/minecraft_server/dimensions mcp/eclipse/Server/src
-cp -a mcp/jars mcp/eclipse/Server
-
-echo Files moved into eclipse/Server/
