0be7b4c761d9cad8aa6f275169ec17550bce7417
phase <ph4sesaber@gmail.com>
2015-05-17 14:16:46 -0700

    Implementation of Server

---
+++ src/main/java/solar/dimensions/DimServer.java
@@ -0,0 +1,29 @@
+package solar.dimensions;
+
+import solar.dimensions.api.Server;
+
+public class DimServer implements Server {
+
+    private String name;
+    private String motd;
+    private final String version;
+
+    public DimServer(String name, String motd, String version){
+        this.name = name;
+        this.motd = motd;
+        this.version = version;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getMOTD(){
+        return motd;
+    }
+
+    public String getVersion(){
+        return version;
+    }
+
+}
