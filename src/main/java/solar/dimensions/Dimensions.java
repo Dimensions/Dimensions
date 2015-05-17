6fcebcb64bf97326ff8593bae50b5734a762706c
phase <ph4sesaber@gmail.com>
2015-05-17 14:26:41 -0700

    Setup singleton and logging

--- src/main/java/solar/dimensions/Dimensions.java
+++ src/main/java/solar/dimensions/Dimensions.java
@@ -1,19 +1,44 @@
 package solar.dimensions;
 
 import java.util.Random;
+import java.lang.IllegalStateException;
 
 import solar.dimensions.api.Game;
 
 public class Dimensions implements Game {
 
-    private static Random rand;
+    private static Random rand = null;
+    private static Dimensions instance = null;
+
+    private DimServer server;
+
+    private Dimensions() {
+        if(instance != null) {
+            throw new IllegalStateException("Instance is already defined");
+        }
+        instance = this;   
+    }
+
+    public DimServer getServer() {
+        return server;
+    }
 
     public static void init() {
-        System.out.println("Initializing Dimensions...");
+        log("Initializing Dimensions...");
         rand = new Random();
+        new Dimensions();
     }
 
-    public static Random getRandom(){
+    public static Random getRandom() {
         return rand;
     }
+
+    public static Dimensions getInstance() {
+        return instance;
+    }
+
+    public static log(String message) {
+        System.out.println("[Dimensions] " + message);
+    }
+
 }
